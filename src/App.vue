1d902a19f560268fbc116725c0fad53274be3e52
sovanda_m <vanda.can.read@gmail.com>
2024-10-11 08:58:29 +0700

    use vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="text-8xl bg-red-600">hello</div>
+  <RouterView />
 </template>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+</script>
+<template>
+  <div class="flex space-x-2">
+    <button @click="count++" class="px-4 py-2 bg-blue-500 rounded-md">
+      Increment
+    </button>
+    <p class="text-4xl">
+      {{ count }}
+    </p>
+  </div>
+</template>
